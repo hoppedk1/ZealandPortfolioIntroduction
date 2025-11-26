a523fd3cb4cdd97f1ec6f71d611043302e8c3fb0
hoppedk1 <73273792+hoppedk1@users.noreply.github.com>
2025-11-26 06:27:38 -0500

    Add files via upload

--- PythonScripts/BegynderPythonScripts/3_FileFinder.py
+++ PythonScripts/BegynderPythonScripts/3_FileFinder.py
@@ -2,14 +2,14 @@ import os
 
 from pathlib import Path
 
-# Specify the directory you want to search in
+# '.' er den nuværende mappe. du kan altid bare skifte det
 search_path = Path('.')
 
-# Iterate and print all .txt files
+# find filer med .txt
 for file in search_path.glob('*.txt'):
     print(file)
 
-""" Faktisk opgaven med os module og AI """ """"
+""" opgaven med os module below""" """"
 search_path = '.' # har bare valgt den nuværende mappe, da /etc ikke findes i windows
 
 for filename in os.listdir(search_path):
